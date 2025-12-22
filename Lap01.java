f067ba7383347ea2f381bf84877de882b3879754
Elaf Alamro <elafealamro@gmail.com>
2025-12-23 02:40:09 +0300

    first

---
+++ Lap01.java
@@ -0,0 +1,13 @@
+/*Write a program that displays “Welcome to Java” in a single line using
+a. b. using a single Java statement
+using three separate Java statements (to print the three words separately followed by a
+new line)*/
+ package javaapplication٢;
+
+ public class Lap01 {
+       public static void main(String[] args) {
+      /*a*/  System.out.println("welcom to java");
+      
+      /*b*/  System.out.println("welcom\n to\n java");
+    }
+}
